ad7cac0ce62cef8edf286fe4b48e9f338fa29dad
Dharmendra Chauhan <dmma359800@gmail.com>
2026-06-02 15:32:20 +0530

    add prototypes.js to demonstrate object creation using prototypes and constructor functions

---
+++ Class/prototypes.js
@@ -0,0 +1,19 @@
+// There is two way create js object 1)using prototypes 2) using classes and contructor
+
+
+// Protypical Method
+
+let createUser = function(name, school, age){
+    this.name = name,
+    this.school = school,
+    this.age = age
+}
+
+createUser.prototype.google = function(){
+    console.log(`Hi bro ${this.name}`)
+     return this.name // eleminate undefined by return the value
+}
+
+const user1 = new createUser("dharmendra")
+console.log(user1.google())
+
